838b519c20bdd7e75ba71da6863cb3dfae160f2f
Vidit <nahbro2511@gmail.com>
2025-06-22 17:58:09 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,13 +16,12 @@ import './App.css';
 
 // Initialize Firebase with config values
 const firebaseConfig = {
-  apiKey: "AIzaSyCZEL-GCj4CR7zadwDi8yAgCk-LIZAuqYc",
-  authDomain: "task-manager-a99bf.firebaseapp.com",
-  projectId: "task-manager-a99bf",
-  storageBucket: "task-manager-a99bf.firebasestorage.app",
-  messagingSenderId: "33906649727",
-  appId: "1:33906649727:web:a94ea90e361c91a0156d4a",
-  measurementId: "G-5BQB1J7VXS"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
